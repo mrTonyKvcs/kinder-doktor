bb470286a6d09f8b74ded1adf480115462add612
tony <attila.kovacs92@gmail.com>
2020-04-28 18:53:14 +0200

    client

--- app/Http/Controllers/CovidController.php
+++ app/Http/Controllers/CovidController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Goutte\Client;
+//use Goutte\Client;
 use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 
@@ -10,13 +10,13 @@ class CovidController extends Controller
 {
     public function index ()
     {
-        $data = [];
-        $url = 'https://koronavirus.gov.hu';
-        $client = new Client();
-        $crawler = $client->request('GET', $url);
-        $data = $crawler->filter('.diagram-a > .number')->each(function ($node) {
-            return str_replace(' ', '', $node->text());
-        });
+        $data = ['2649', '516', '0', '67172'];
+        //$url = 'https://koronavirus.gov.hu';
+        //$client = new Client();
+        //$crawler = $client->request('GET', $url);
+        //$data = $crawler->filter('.diagram-a > .number')->each(function ($node) {
+            //return str_replace(' ', '', $node->text());
+        //});
 
         return view('covid.index', compact('data'));
     }
